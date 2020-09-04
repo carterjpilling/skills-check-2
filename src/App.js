e22ee65f3afd3422999004f3461dff50f348100b
Cj <carterjpilling@gmail.com>
2020-09-04 11:15:07 -0600

    finished step 4

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import Dashboard from './Components/Dashboard/Dashboard'
 import Form from './Components/Form/Form'
 import Header from './Components/Header/Header'
+import Axios from 'axios';
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      products:
-        [{ name: 'apple', price: 3, product_img: 'asdf' }, { name: 'grape', price: 3, product_img: 'asdf' }, { name: 'banana', price: 3, product_img: 'asdf' }]
-
-
+      products: []
     }
   }
+  componentDidMount() {
+    this.getInventory()
+  }
+  getInventory() {
+    Axios.get('/api/inventory')
+      .then(res => this.setState({ products: res.data }))
+  }
+
+
   render() {
     return (
       <div className="App" >
